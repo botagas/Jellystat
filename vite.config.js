33d376d2fc516f13bf7769e777b041c8f5ae7e8c
Patrick <49989644+botagas@users.noreply.github.com>
2025-03-30 19:23:37 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+require("dotenv").config();
+
+const port = process.env.JS_PORT || 3000;
+const proxyTarget = `http://127.0.0.1:${port}`;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: "JS_",
@@ -18,16 +23,16 @@ export default defineConfig({
     port: 3000,
     // port for exposing APIs
     proxy: {
-      "/api": "http://127.0.0.1:3000",
-      "/proxy": "http://127.0.0.1:3000",
-      "/stats": "http://127.0.0.1:3000",
-      "/sync": "http://127.0.0.1:3000",
-      "/auth": "http://127.0.0.1:3000",
-      "/backup": "http://127.0.0.1:3000",
-      "/logs": "http://127.0.0.1:3000",
-      "/socket.io": "http://127.0.0.1:3000",
-      "/swagger": "http://127.0.0.1:3000",
-      "/utils": "http://127.0.0.1:3000",
+      "/api": proxyTarget,
+      "/proxy": proxyTarget,
+      "/stats": proxyTarget,
+      "/sync": proxyTarget,
+      "/auth": proxyTarget,
+      "/backup": proxyTarget,
+      "/logs": proxyTarget,
+      "/socket.io": proxyTarget,
+      "/swagger": proxyTarget,
+      "/utils": proxyTarget,
     },
   },
   target: ["es2015"],
